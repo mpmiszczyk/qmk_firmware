9e7a89fa48b5d2883d62494fb24e26330c279373
marcin miszczyk <mpmiszczyk@gmail.com>
2019-06-24 17:20:29 +0200

    [Keymap] Move shift to thumb clusters.  Mostly righd thumb in use.

--- keyboards/hotdox/keymaps/yardstick/keymap.c
+++ keyboards/hotdox/keymaps/yardstick/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Esc/~  |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   q  |   w  |   e  |   r  |   t  |  L1  |           |  L1  |   y  |   u  |   i  |   o  |   p  |   \    |
+ * | Back   |   q  |   w  |   e  |   r  |   t  |  L1  |           |  L1  |   y  |   u  |   i  |   o  |   p  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |Space/ C|   a  |   s  |   d  |   f  |   g  |------|           |------|   h  |   j  |   k  |   l  |; / L2| ' / C  |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
@@ -25,10 +25,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                        | LGui |App/ M|       | M    |Esc/ C|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | BackS| Space|------|       |------|  Tab   |Enter |
+ *                                 |Bck/lS| Space|------|       |------| Tab/rS |Enter |
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -36,22 +36,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_GESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_DELT,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
+        KC_BSPC,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         CTL_T(KC_SPC),   KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,         RALT_T(KC_Z), KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
         LT(SYMB,KC_GRV), KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
-                                               ALT_T(KC_APP),  KC_LGUI,
+
+                                                      KC_LGUI, ALT_T(KC_APP),
                                                                KC_HOME,
-                                             KC_BSPC, KC_SPC,  KC_END,
+                                     LSFT_T(KC_BSPC), KC_SPC,  KC_END,
+
         // right hand
              KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),CTL_T(KC_QUOT),
-             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, RALT_T(KC_SLSH),   KC_RSFT,
+             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, RALT_T(KC_SLSH),  KC_RSFT,
                                   KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
+
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
-             KC_PGDN,KC_TAB,KC_ENT
+             KC_PGDN, RSFT_T(KC_TAB), KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
