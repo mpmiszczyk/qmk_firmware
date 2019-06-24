fe9774be572f849159bdad8e11c0a88c2c6acff9
marcin miszczyk <mpmiszczyk@gmail.com>
2019-06-24 14:11:21 +0200

    [Keymap] Introduce RightAlt for both hands
    Since I still will have to write in Polish, I will have to use Right
    Alt quite often for all the `ą ć ę ł ń ż ź` letters.

--- keyboards/hotdox/keymaps/yardstick/keymap.c
+++ keyboards/hotdox/keymaps/yardstick/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |Space/ C|   a  |   s  |   d  |   f  |   g  |------|           |------|   h  |   j  |   k  |   l  |; / L2| ' / C  |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * |   lS   | z/ C |   x  |   c  |   v  |   b  |      |           |      |   n  |   m  |   ,  |   .  | // C |   rS   |
+ * |   lS   |z/rAlt|   x  |   c  |   v  |   b  |      |           |      |   n  |   m  |   ,  |   .  |//rAlt|   rS   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         KC_DELT,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         CTL_T(KC_SPC),   KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,         CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_LSFT,         RALT_T(KC_Z), KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
         LT(SYMB,KC_GRV), KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
                                                ALT_T(KC_APP),  KC_LGUI,
                                                                KC_HOME,
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),CTL_T(KC_QUOT),
-             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, RALT_T(KC_SLSH),   KC_RSFT,
                                   KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
